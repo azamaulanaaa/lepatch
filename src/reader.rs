048094c1d17517ce0fd1f1446bcc809d0dc728d1
Aza Maulana <azamaulanaaa@gmail.com>
2025-12-25 15:34:38 +0700

    feat(reader): add Slice Reader

--- src/reader.rs
+++ src/reader.rs
@@ -1,4 +1,12 @@
-use std::{fs::File, io, ops::Deref};
+#[cfg(windows)]
+use std::os::windows::fs::FileExt as WinFileExt;
+
+use std::{
+    fs::File,
+    io::{self, Read},
+    ops::Deref,
+    sync::Arc,
+};
 
 use fs2::FileExt;
 
@@ -25,5 +33,48 @@ impl Deref for FileLock {
 
     fn deref(&self) -> &Self::Target {
         &self.inner
+    }
+}
+
+pub struct SliceReader {
+    file: Arc<FileLock>,
+    offset: u64,
+    remaining: u64,
+}
+
+impl SliceReader {
+    pub fn new(file: Arc<FileLock>, offset: u64, limit: u64) -> io::Result<Self> {
+        Ok(Self {
+            file,
+            offset,
+            remaining: limit,
+        })
+    }
+}
+
+impl Read for SliceReader {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        if self.remaining == 0 {
+            return Ok(0);
+        }
+
+        let max_len = self.remaining.min(buf.len() as u64) as usize;
+        let read_buf = &mut buf[..max_len];
+
+        #[cfg(unix)]
+        let n = self.file.read_at(read_buf, self.offset)?;
+
+        #[cfg(windows)]
+        let n = self.file.seek_read(read_buf, self.offset)?;
+
+        if n == 0 {
+            self.remaining = 0;
+            return Ok(0);
+        }
+
+        self.offset += n as u64;
+        self.remaining -= n as u64;
+
+        Ok(n)
     }
 }
