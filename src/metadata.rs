642a53148195296c771ba2637a1d1e2cf12706da
Aza Maulana <azamaulanaaa@gmail.com>
2025-12-27 02:30:02 +0700

    feat(metadata): add basic structure

---
+++ src/metadata.rs
@@ -0,0 +1,33 @@
+use std::path::PathBuf;
+
+pub struct Snapshot {
+    pub name: String,
+    pub version: u8,
+    pub files: Vec<File>,
+    pub chunks: Vec<Chunk>,
+    pub file_chunks: Vec<FileChunk>,
+    pub file_symlink: Vec<FileSymlink>,
+}
+
+pub struct Chunk {
+    pub hash: Vec<u8>,
+    pub location: String,
+}
+
+pub struct FileSymlink {
+    pub path: PathBuf,
+    pub source: PathBuf,
+    pub is_hard: bool,
+}
+
+pub struct File {
+    pub path: PathBuf,
+}
+
+pub struct FileChunk {
+    pub chunk_index: u32,
+    pub file_index: u32,
+    pub chunk_offset: u32,
+    pub file_offset: u64,
+    pub length: u32,
+}
